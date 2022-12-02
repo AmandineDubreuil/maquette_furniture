a1b74aa1269752c043b9ef8fb4bba8ee710b7f2a
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2022-12-02 15:05:15 +0100

    end responsive and begin burger mobile

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// console.log("connected");
+
+// const burger = document.getElementById("burger");
+// const navUl = document.getElementsByClassName("navUl");
+
+// burger.addEventListener("click", function () {
+//   document.navUl.classList.toggle("clickUl");
+//   document.navUl.classList.toggle("noclickUl");
+// });
